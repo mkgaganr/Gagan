a453369c75664bc4e08cdec20a667319ba669fb7
M K GAGAN ROSHAN <53825035+mkgaganr@users.noreply.github.com>
2019-11-16 12:59:22 +0530

    Update package.java

--- package.java
+++ package.java
@@ -22,28 +22,4 @@ public static void main(String args[])
       ex[i].display();
    }
     }
- }import CIE.*;
-import SEE.*;
-import java.util.*;
-class package
-{
-public static void main(String args[])
-{
-     
-      Scanner sc=new Scanner(System.in);
-       System.out.println("enter the no of student");
-        int n=sc.nextInt();
- Internal in[]=new Internal[n];
-  External ex[]=new External[n];
-      for(int i=0;i<n;i++)
-      {   
-      
-          in[i]=new Internal();
-          in[i].getdata();
-          in[i].display(); 
-	ex[i]=new External();
-      ex[i].getdata();
-      ex[i].display();
-   }
-    }
  }
